1c6a1db996bb4ec301c7ebd44740e04b44a5426b
KgothatsoTheko <kgothatsotheko7@gmail.com>
2025-05-15 14:32:16 +0200

    updated and deployed

--- src/app/components/testimonials/testimonials.component.ts
+++ src/app/components/testimonials/testimonials.component.ts
@@ -9,19 +9,19 @@ export class TestimonialsComponent {
 
   testimonials = [
     {
-      image: "https://focusednetwork.co.za/wp-content/uploads/2022/05/Shirley-Nhlengethwa-2.jpg",
+      image: "https://media.licdn.com/dms/image/v2/C4D03AQFYrnRVNCwWIw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1652208834984?e=1752710400&v=beta&t=JeXUlrsC3hBdw_3rnEZf6kzU6c-ODNaDjR7ckc0d0c0",
       text: "Amazing work on our mobile application. Always communicated and kept up to date on progress of project to complete. Definitely doing business again in future for updates and new features.",
       name: "Shirley Nhlengethwa",
       position: "Focused Network Founder & Chairperson"
     },
     {
-      image: "https://via.placeholder.com/200",
+      image: "https://ionicframework.com/docs/img/demos/avatar.svg",
       text: "In Business",
       name: "In Business",
       position: "In Business"
     },
     {
-      image: "https://via.placeholder.com/200",
+      image: "https://ionicframework.com/docs/img/demos/avatar.svg",
       text: "In Business",
       name: "In Business",
       position: "In Business"
